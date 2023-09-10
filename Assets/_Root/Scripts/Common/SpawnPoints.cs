8b0416296d2102ef6c6ac4664b769a3dcccc0437
= <=>
2023-09-11 00:35:46 +0700

    Add spawn points

---
+++ Assets/_Root/Scripts/Common/SpawnPoints.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpawnPoints : MonoBehaviour, IReset
+{
+    [SerializeField]
+    protected List<Transform> points = new List<Transform>();
+
+    public void Reset()
+    {
+        this.LoadPoints();
+    }
+
+    protected virtual void LoadPoints()
+    {
+        if (this.points.Count > 0)
+        {
+            return;
+        }
+
+        foreach (Transform point in transform)
+        {
+            this.points.Add(point);
+        }
+    }
+
+    public virtual Transform GetRandom()
+    {
+        int random = Random.Range(0, this.points.Count);
+        return this.points[random];
+    }
+}
---
+++ Assets/_Root/Scripts/Junk/JunkRandom.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkRandom : MonoBehaviour, IReset
+{
+    [SerializeField]
+    protected JunkController junkController;
+
+    public void Reset()
+    {
+        this.LoadJunkSpawner();
+    }
+
+    protected virtual void LoadJunkSpawner()
+    {
+        if (this.junkController != null)
+        {
+            return;
+        }
+        this.junkController = GetComponent<JunkController>();
+    }
+
+    private void Start()
+    {
+        this.JunkSpawning();
+    }
+
+    protected virtual void JunkSpawning()
+    {
+        Transform randomPoint = this.junkController.JunkSpawnPoints.GetRandom();
+
+        Vector3 position = randomPoint.position;
+        Quaternion rotation = randomPoint.rotation;
+
+        Transform junk = this.junkController.JunkSpawner.Spawn(JunkSpawner.JunkOne, position, rotation);
+        junk.gameObject.SetActive(true);
+
+        Invoke(nameof(this.JunkSpawning), 1f);
+    }
+}
